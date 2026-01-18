a15f3e2b3c68f62359f7098289de76ae379b4e0b
derekr06 <derekkrios10@gmail.com>
2026-01-18 12:28:53 -0600

    Add Terraform VPC, subnets, and Internet Gateway

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "sre-platform-vpc"
+  }
+}
+
+
+# Public Subnet
+resource "aws_subnet" "public1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "sre-public-subnet-1"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "sre-private-subnet-1"
+  }
+}
+
+#create internet gateway 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "sre-igw"
+  }
+}
+
+# create route table for public subnet 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "sre-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
